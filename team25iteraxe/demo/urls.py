8f3d16fff620f9862dcef0bd1fc736137175a853
Timothée Gaultier <tiimogo@gmail.com>
2021-10-12 17:00:55 +0200

    main stuff

--- team25iteraxe/demo/urls.py
+++ team25iteraxe/demo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('archive/<str:pk>', views.subject_detail, name='details')
 ]
--- team25iteraxe/demo/views.py
+++ team25iteraxe/demo/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Subject
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request): # list
+    subject_list = Subject.objects.all()
+    return render(request, 'archive_list.html', {'subject_list': subject_list})
+
+
+def subject_detail(request, pk):
+    subject_detail = get_object_or_404(Subject, pk=pk)
+    return render(request, 'archive_single.html', {'subject': subject_detail})
