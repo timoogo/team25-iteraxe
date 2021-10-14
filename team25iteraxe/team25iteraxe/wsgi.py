06f781d3c63a75cf4bc81e2fd8d12a11ac36e265
Timothée Gaultier <tiimogo@gmail.com>
2021-10-15 00:21:22 +0200

    try deploy scalingo

--- team25iteraxe/team25iteraxe/wsgi.py
+++ team25iteraxe/team25iteraxe/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team25iteraxe.settings')
 
-application = get_wsgi_application()
